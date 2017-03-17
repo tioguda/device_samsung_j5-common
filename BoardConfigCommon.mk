6a8fb5d787637ecfbe6faf2125dee78333572e6b
SoUnd001 <sound0020@gmail.com>
2017-03-17 22:53:54 +0200

    j5-common : Use legacy camera hal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,12 +124,7 @@ BOARD_VOLD_MAX_PARTITIONS := 65
 # Camera
 TARGET_PROVIDES_CAMERA_HAL := true
 USE_DEVICE_SPECIFIC_CAMERA := true
-# Reference for Gralloc. In case the above do not work, we try those too.
-#TARGET_USE_COMPAT_GRALLOC_ALIGN := true
-#TARGET_USE_COMPAT_GRALLOC_PERFORM := true
-#BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
-#TARGET_HAS_LEGACY_CAMERA_HAL1 := true
-#TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
 # Workaround to avoid issues with legacy liblights on QCOM platforms
 TARGET_PROVIDES_LIBLIGHT := true
