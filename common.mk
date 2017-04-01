de1c4372dd6f9e1492e10789bd7fa5a705081b14
SoUnd001 <sound0020@gmail.com>
2017-04-01 19:31:13 +0300

    Revert "j5-common: Inprove camera"
    This reverts commit 53f563af227609ae45a8c3f5238c8d60e32dec7f.

--- common.mk
+++ common.mk
@@ -272,6 +272,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
     media.stagefright.less-secure=true
 
