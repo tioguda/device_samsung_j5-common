3a63fe5a756915d1e2bb2954c4f2b9030b095b3b
SoUnd001 <sound0020@gmail.com>
2017-04-08 11:58:32 +0300

    j5-common: Let's not force api1 for cam2

--- common.mk
+++ common.mk
@@ -256,7 +256,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1 \
     media.stagefright.legacyencoder=true \
     media.stagefright.less-secure=true
 
